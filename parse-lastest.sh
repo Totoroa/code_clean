fe3c4566c59494e82894d6ece62e1894e40f655e
Totoroa <song.xiaonan@foxmail.com>
2020-06-06 23:38:26 +0800

    Add files via upload

--- parse-lastest.sh
+++ parse-lastest.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
 
 # function path.
-sameFuncPath=E:/lab/SARD/functions/Bad/same
-diffFuncPath=E:/lab/SARD/functions/Bad/diff
-linePath=E:/lab/SARD/functions/BadDpd
-Flag=vul
+#sameFuncPath=E:/lab/SARD/functions/Bad/same
+#diffFuncPath=E:/lab/SARD/functions/Bad/diff
+sameFuncPath=C:/Users/admin/Desktop/workspace-python/cleancode/openssl-1.0.0a/functions/same
+diffFuncPath=C:/Users/admin/Desktop/workspace-python/cleancode/openssl-1.0.0a/functions/diff
+linePath=C:/Users/admin/Desktop/workspace-python/cleancode/openssl-1.0.0a/func_dpd
+Flag=src
 
-start=$(date +%s)
+#start=$(date +%s)
 
 # process same name functions
-dir=$(ls -l $sameFuncPath |awk '/^-/ {print $NF}')
-for i in $dir
-do
+#dir=$(ls -l $sameFuncPath |awk '/^-/ {print $NF}')
+#for i in $dir
+#do
     #echo $sameFuncPath'/'$i
-	./joern-parse $sameFuncPath'/'$i --out samecpg.bin
-	./joern --script E:/joern/joern-cli/graph/f-pdg.sc --params cpgFile=samecpg.bin,inFile=$sameFuncPath'/'$i,outFile=$linePath,flag=$Flag
-done 
+#	./joern-parse $sameFuncPath'/'$i --out samecpg.bin
+#	./joern --script E:/joern/joern-cli/graph/f-pdg.sc --params cpgFile=samecpg.bin,inFile=$sameFuncPath'/'$i,outFile=$linePath,flag=$Flag
+#done 
 
 # process diff name funcions
-dir1=$(ls -l $sameFuncPath |awk '/^d/ {print $NF}')
+dir1=$(ls -l $diffFuncPath |awk '/^d/ {print $NF}')
 for j in $dir1
 do
     #echo $diffFuncPath'/'$j
 	./joern-parse $diffFuncPath'/'$j --out diffcpg.bin
-	./joern --script E:/joern/joern-cli/graph/total-pdg.sc --params cpgFile=diffcpg.bin,inFile=$diffFuncPath,outFile=$linePath,flag=$Flag
+	./joern --script E:/joern/joern-cli/graph/total-pdg.sc --params cpgFile=diffcpg.bin,inFile=$diffFuncPath'/'$j,outFile=$linePath,flag=$Flag
 done
 
 end=$(date +%s)
